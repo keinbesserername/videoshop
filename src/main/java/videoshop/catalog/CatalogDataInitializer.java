28ed03d1647219dd9790d91a29f8bd1c14b48d06
keinbesserername <leontilokunze@gmail.com>
2022-04-17 23:46:17 +0200

    consistency change

--- src/main/java/videoshop/catalog/CatalogDataInitializer.java
+++ src/main/java/videoshop/catalog/CatalogDataInitializer.java
@@ -62,16 +62,16 @@ class CatalogDataInitializer implements DataInitializer {
 
 		LOG.info("Creating default catalog entries.");
 
-		videoCatalog.save(new Disc("Last Action Hero", "lac", Money.of(100, EURO), "Äktschn/Comedy", DiscType.DVD));
+		videoCatalog.save(new Disc("Last Äktschn Hero", "lac", Money.of(100, EURO), "Äktschn/Comedy", DiscType.DVD));
 		videoCatalog.save(new Disc("Back to the Future", "bttf", Money.of(9.99, EURO), "Sci-Fi", DiscType.DVD));
 		videoCatalog.save(new Disc("Fido", "fido", Money.of(9.99, EURO), "Comedy/Drama/Horror", DiscType.DVD));
-		videoCatalog.save(new Disc("Super Fuzz", "sf", Money.of(9.99, EURO), "Action/Sci-Fi/Comedy", DiscType.DVD));
+		videoCatalog.save(new Disc("Super Fuzz", "sf", Money.of(9.99, EURO), "Äktschn/Sci-Fi/Comedy", DiscType.DVD));
 		videoCatalog.save(new Disc("Armour of God II: Operation Condor", "aog2oc", Money.of(14.99, EURO),
-				"Action/Adventure/Comedy", DiscType.DVD));
+				"Äktschn/Adventure/Comedy", DiscType.DVD));
 		videoCatalog.save(new Disc("Persepolis", "pers", Money.of(14.99, EURO), "Animation/Biography/Drama", DiscType.DVD));
 		videoCatalog
-				.save(new Disc("Hot Shots! Part Deux", "hspd", Money.of(9999.0, EURO), "Action/Comedy/War", DiscType.DVD));
-		videoCatalog.save(new Disc("Avatar: The Last Airbender", "tla", Money.of(19.99, EURO), "Animation/Action/Adventure",
+				.save(new Disc("Hot Shots! Part Deux", "hspd", Money.of(9999.0, EURO), "Äktschn/Comedy/War", DiscType.DVD));
+		videoCatalog.save(new Disc("Avatar: The Last Airbender", "tla", Money.of(19.99, EURO), "Animation/Äktschn/Adventure",
 				DiscType.DVD));
 
 		videoCatalog.save(new Disc("Secretary", "secretary", Money.of(6.99, EURO), "Political Drama", DiscType.BLURAY));
@@ -82,9 +82,9 @@ class CatalogDataInitializer implements DataInitializer {
 				.save(new Disc("The Princess Bride", "tpb", Money.of(39.99, EURO), "Adventure/Comedy/Family", DiscType.BLURAY));
 		videoCatalog.save(new Disc("Top Secret!", "ts", Money.of(39.99, EURO), "Comedy", DiscType.BLURAY));
 		videoCatalog
-				.save(new Disc("The Iron Giant", "tig", Money.of(34.99, EURO), "Animation/Action/Adventure", DiscType.BLURAY));
-		videoCatalog.save(new Disc("Battle Royale", "br", Money.of(19.99, EURO), "Action/Drama/Thriller", DiscType.BLURAY));
-		videoCatalog.save(new Disc("Oldboy", "old", Money.of(24.99, EURO), "Action/Drama/Thriller", DiscType.BLURAY));
+				.save(new Disc("The Iron Giant", "tig", Money.of(34.99, EURO), "Animation/Äktschn/Adventure", DiscType.BLURAY));
+		videoCatalog.save(new Disc("Battle Royale", "br", Money.of(19.99, EURO), "Äktschn/Drama/Thriller", DiscType.BLURAY));
+		videoCatalog.save(new Disc("Oldboy", "old", Money.of(24.99, EURO), "Äktschn/Drama/Thriller", DiscType.BLURAY));
 		videoCatalog.save(new Disc("Bill & Ted's Excellent Adventure", "bt", Money.of(29.99, EURO),
 				"Adventure/Comedy/Family", DiscType.BLURAY));
 	}
